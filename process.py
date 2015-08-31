9055cd9c60c08063c53cb43ea83a13dd90c6ae41
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-08-31 22:39:43 +0200

    minor changes to process module

--- process.py
+++ process.py
@@ -6,5 +6,6 @@ import clusters
 rownames, colnames, data = clusters.readfile("data.txt")
 kclust = clusters.kcluster(data, k=10)
 
-for cluster in kclust:
+for idx, cluster in enumerate(kclust):
+    print("Cluster {}:".format(idx))
     print([rownames[r] for r in cluster])
