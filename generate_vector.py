a18940c698ef9b86c70a0f49e8caadc9b7fb23e8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-09-01 07:07:18 +0200

    Added usage() function and improved management of parameters.

--- generate_vector.py
+++ generate_vector.py
@@ -26,7 +26,7 @@ def remove_stopwords(words):
     """
     return [w for w in words if w not in STOP_WORDS]
 
-def getwordcounts(talk):
+def get_word_counts(talk):
     # Parse the feed
     wc = {}
     # Extract a list of words
@@ -37,14 +37,21 @@ def getwordcounts(talk):
         wc[word] += 1
     return talk["id"] , wc
 
-def generate_vectors(url, nickname, password):
+def retrieve_data(url, nickname, password):
     r = requests.get(url, auth=(nickname, password))
+    if r.status_code != 200:
+        print("Problem when connecting to {}: {}".format(url, r.reason))
+        sys.exit(1)
     talks = json.loads(r.text)
+    return talks
+
+def generate_vectors(url, nickname, password):
+    talks = retrieve_data(url, nickname, password)
 
     apcount={}
     wordcounts={}
     for talk in talks:
-        talk_id, wc = getwordcounts(talk)
+        talk_id, wc = get_word_counts(talk)
         wordcounts[talk_id] = wc
         for word, count in wc.items():
           apcount.setdefault(word,0)
@@ -53,9 +60,9 @@ def generate_vectors(url, nickname, password):
 
     wordlist=[]
     for w,bc in apcount.items():
-      frac = float(bc)/len(talks)
-      if frac>0.1 and frac<0.5:
-        wordlist.append(w)
+        frac = float(bc)/len(talks)
+        if frac>0.1 and frac<0.5:
+            wordlist.append(w)
 
     out = open('data.txt','w')
     out.write('id')
@@ -63,16 +70,32 @@ def generate_vectors(url, nickname, password):
         out.write('\t%s' % word)
     out.write('\n')
     for talk_id, wc in wordcounts.items():
-      out.write(talk_id)
-      for word in wordlist:
-        if word in wc:
-            out.write('\t%d' % wc[word])
-        else:
-            out.write('\t0')
-      out.write('\n')
+        out.write(talk_id)
+        for word in wordlist:
+            if word in wc:
+                out.write('\t%d' % wc[word])
+            else:
+                out.write('\t0')
+        out.write('\n')
+    print("vectors saved")
 
-
+def usage(code=1):
+    """
+    Display usage information.
+    """
+    print("Usage: generate_vector.py nickname password [service_url]")
+    return code
 
 if __name__ == "__main__":
-    generate_vectors("http://european-data-forum.list.lu/api/v1.0/talks.json",
-                        sys.argv[1], sys.argv[2])
+    # Point of entry in execution mode
+    try:
+        if sys.argv[1] == "--help":
+            sys.exit(usage(0))
+        nickname, password = sys.argv[1], sys.argv[2]
+    except Exception as e:
+        sys.exit(usage())
+    try:
+        service_url = sys.argv[3]
+    except:
+        service_url = "http://european-data-forum.list.lu/api/v1.0/talks.json"
+    generate_vectors(service_url, nickname, password)
